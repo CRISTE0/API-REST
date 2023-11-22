f54b677b00078256f9d5d33dbf8f2af06ac7534d
CRISTE0 <escuderocristian65@gmail.com>
2023-11-22 14:11:54 -0500

    api

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require('mongoose');
+require('dotenv').config();
+const userRoute = require("./routes/user")
+
+// settings
+const app = express();
+const port = process.env.PORT || 3001;
+
+// middlewares
+app.use(express.json());
+app.use("/api",userRoute);
+
+// routes
+app.get("/",(req,res)=>{
+    res.send({message:"Bienvenido to my API"})
+});
+
+// mongodb connection
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(()=> console.log("Conectado a mongo atlas"))
+.catch((error)=> console.error(error));
+
+
+// server listening
+app.listen(port,()=> console.log("serve listening",port));
