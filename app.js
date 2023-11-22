6574ef6ec05d3e4a37c66dcbc7cf941dcf9e42b4
CRISTE0 <escuderocristian65@gmail.com>
2023-11-22 14:53:19 -0500

    siuuu

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use("/api",userRoute);
 
 // routes
 app.get("/",(req,res)=>{
-    res.send({message:"Bienvenido to my API"})
+    res.send("Bienvenido to my API")
 });
 
 // mongodb connection
